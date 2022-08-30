d27651bba4d1e8b0ec2318abbd455e1107129180
E-Lop <edmondolopez@gmail.com>
2022-08-30 15:49:30 +0200

    added ComicsController index view

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('main_content')
+    <h1>Ecco i nostri fumetti</h1>
+    
+    @foreach ($comics as $comic)
+        <div>
+            <div>Titolo: {{$comic->title}}</div>
+            <div>Serie: {{$comic->series}}</div>
+            <div>Data di pubblicazione: {{$comic->sale_date}}</div>
+            <div>Prezzo: {{$comic->price}}</div>
+            <div>Descrizione: {{$comic->description}}</div>
+        </div>
+    @endforeach
+@endsection
