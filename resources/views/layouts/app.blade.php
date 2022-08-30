bf671fb08dacc5acca74e214171f01bb1d17d2c7
E-Lop <edmondolopez@gmail.com>
2022-08-30 14:32:43 +0200

    creted layout app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta main_content="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DC Comics</title>
+</head>
+<body>
+    <header>
+        Header
+    </header>
+
+    <main>
+        @yield('main_content')
+    </main>
+</body>
+</html>
