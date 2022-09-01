6b9a8602dbb3ed83acd228ec5d4a70bc38b808fa
E-Lop <edmondolopez@gmail.com>
2022-09-01 17:28:23 +0200

    error message under element in create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,10 +2,10 @@
 
 @section('main_content')
     <div class="container">
-        <h1 class="text-center">Crea un nuovo prodotto</h1>
+        <h1 class="text-center">Crea un nuovo fumetto</h1>
 
         {{-- gestione messaggi di errore --}}
-        @if ($errors->any())
+        {{-- @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
                     @foreach ($errors->all() as $error)
@@ -13,7 +13,7 @@
                     @endforeach
                 </ul>
             </div>
-        @endif
+        @endif --}}
         
         <form action="{{ route('comics.store') }}" method="post">
             @csrf
@@ -22,37 +22,57 @@
                 <label for="title" class="form-label">Titolo</label>
                 <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
             </div>
-            
+            @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
             
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
                 <textarea name="description" class="form-control" id="description" cols="30" rows="10">{{ old('description') }}</textarea>
             </div>
+            @error('description')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
             
             <div class="mb-3">
                 <label for="thumb" class="form-label">Url immagine</label>
                 <input type="text" class="form-control" id="thumb" name="thumb" value="{{ old('thumb') }}">
             </div>
+            @error('thumb')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
             
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
                 <input type="text" class="form-control" id="price" name="price" value="{{ old('price') }}">
             </div>
+            @error('price')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
             
             <div class="mb-3">
                 <label for="series" class="form-label">Serie</label>
                 <input type="text" class="form-control" id="series" name="series" value="{{ old('series') }}">
             </div>
+            @error('series')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
             
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Data di pubblicazione</label>
                 <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{ old('sale_date') }}">
             </div>
+            @error('sale_date')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
             
             <div class="mb-3">
                 <label for="type" class="form-label">Tipo di prodotto</label>
                 <input type="text" class="form-control" id="type" name="type" value="{{ old('type') }}">
             </div>
+            @error('type')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
             
 
             <input type="submit" class="btn btn-primary" value="Salva">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,8 @@
               <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                   <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
-                  <a class="nav-link" href="{{ route('comics.index') }}">Comics</a>
-                  <a class="nav-link" href="{{ route('comics.create') }}">Nuovo prodotto</a>                  
+                  <a class="nav-link" href="{{ route('comics.index') }}">Fumetti</a>
+                  <a class="nav-link" href="{{ route('comics.create') }}">Nuovo fumetto</a>                  
                 </div>
               </div>
             </div>
